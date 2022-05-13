b7ac38d520446caec6e1c439efa1cf9ecaa8d595
Carlos Garcia <carlos.garcia@nextgenpty.com>
2022-05-12 19:47:26 -0500

    Add chomper enemy to scene and basic movement

--- src/scenes/scene1.js
+++ src/scenes/scene1.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import backgroundImg from '../assets/backgrounds/background3.png'
+import enemyImg from '../assets/enemies/chomper.png'
 
 export class Scene1 extends Phaser.Scene {
   constructor() {
@@ -8,7 +9,16 @@ export class Scene1 extends Phaser.Scene {
 
   preload() {
     this.add.text(20, 20, 'Game Loading...');
-    this.load.image('bg', backgroundImg)
+    this.load.image('bg', backgroundImg);
+
+    this.load.spritesheet(
+      'enemy',
+      enemyImg,
+      {
+        frameWidth: 114,
+        frameHeight: 114
+      }
+    );
 
     setTimeout(() => {
       this.scene.start('playGame');
--- src/scenes/scene2.js
+++ src/scenes/scene2.js
@@ -6,12 +6,37 @@ export class Scene2 extends Phaser.Scene {
   }
 
   create() {
-    this.background = this.add.image(0, 0, 'bg')
-    this.background.setOrigin(0, 0)
+    this.background = this.add.tileSprite(0, 0, this.width, this.height, 'bg');
+    this.background.setOrigin(0, 0);
+
+    this.enemy = this.add.sprite(
+      this.background.width,
+      this.background.height / 2,
+      'enemy'
+    )
 
     this.add.text(20, 20, 'Playing Game...', {
       font: '25px Arial',
       color: 'yellow'
-      })
+    });
   }
+
+  update() {
+    this.background.tilePositionX += 1;
+    this.moveEnemy(this.enemy, 2);
+  }
+
+  moveEnemy(enemy, speed) {
+    enemy.x -= speed;
+    if(enemy.x <= 0) {
+      this.resetEnemyPosition(enemy);
+    }
+  }
+
+  resetEnemyPosition(enemy) {
+    enemy.x = this.background.width;
+    var randomY = Phaser.Math.Between(0, this.background.height);
+    enemy.y = randomY;
+  }
+
 }
